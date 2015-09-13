6cbead28dd1a5a9958d4f1e60d6cd93e1651488f
absimaldata <adpq11@gmail.com>
2015-09-13 22:29:24 +0530

    Double Rook Bonus Take 2

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -184,7 +184,7 @@ namespace {
   const Score Unstoppable        = S( 0, 20);
   const Score Hanging            = S(31, 26);
   const Score PawnAttackThreat   = S(20, 20);
-  const Score Drook              = S(25, 20);
+  const Score Drook              = S(37, 15);
   // Penalty for a bishop on a1/h1 (a8/h8 for black) which is trapped by
   // a friendly pawn on b2/g2 (b7/g7 for black). This can obviously only
   // happen in Chess960 games.
