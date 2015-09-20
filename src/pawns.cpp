02f79bf17d3e65d30d0adeb4971e1e5b356d5975
absimaldata <adpq11@gmail.com>
2015-09-21 01:33:56 +0530

    Shelter Weakness Take 1

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -271,7 +271,7 @@ Value Entry::shelter_storm(const Position& pos, Square ksq) {
       b  = theirPawns & file_bb(f);
       Rank rkThem = b ? relative_rank(Us, frontmost_sq(Them, b)) : RANK_1;
 
-      safety -=  ShelterWeakness[std::min(f, FILE_H - f)][rkUs]
+      safety -=  ShelterWeakness[std::min(f, FILE_H - f)][rkUs] / 3
                + StormDanger
                  [f == file_of(ksq) && rkThem == relative_rank(Us, ksq) + 1 ? BlockedByKing  :
                   rkUs   == RANK_1                                          ? NoFriendlyPawn :
